b07372c67a1d74ba4f6eb0931b857ca6601fd4d8
Andrian Putra Ramadan <ramadanandrian89@gmail.com>
2023-06-28 23:59:04 +0700

    chore: some configuration in env

--- src/config/env.js
+++ src/config/env.js
@@ -1,10 +1,3 @@
-const dotenv = require("dotenv");
-const path = require("path");
-
-const result = dotenv.config({ path: path.resolve(__dirname, "../../.env") });
-
-if (result.error) throw result.error;
-
-const envs = result.parsed;
-
-module.exports = envs;
+require("dotenv").config();
+const env = process.env;
+module.exports = env;
