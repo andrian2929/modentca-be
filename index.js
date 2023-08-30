25f98806671163e2844da8568cc61b4d0ca226f2
Andrian Putra Ramadan <ramadanandrian89@gmail.com>
2023-08-30 12:17:39 +0700

    chore: update cors in index

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.use(cors({ credentials: true }))
+app.use(cors({ origin: '*' }))
 app.use(compression())
 app.use(cookieParser())
 app.use(express.json())
